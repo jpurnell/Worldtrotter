96e57b8af11584f6cc346d90c74c8c0406c78ca1
Justin Purnell <jpurnell@Swoop.local>
2017-01-18 10:04:45 -0500

    first commit

---
+++ ConversionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Justin Purnell on 1/9/17.
+//  Copyright © 2017 Justin Purnell. All rights reserved.
+//
+
+import Foundation
---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Justin Purnell on 1/10/17.
+//  Copyright © 2017 Justin Purnell. All rights reserved.
+//
+
+import Foundation
